c2eb864d2c723549cabd6a4ae71b2e7020b6ba0a
Діма Калинюк <dima.kalinyuk.2019@ukr.net>
2020-06-09 13:03:45 +0300

    Laba3.1

---
+++ Laba3Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Question2336627
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "Видаляє всi слова, передостання лiтера яких голосна";
+            var pattern = @"\b[-\p{IsCyrillic}]*?[аєєиіїоуюя][-\p{IsCyrillic}]\b";
+            var str2 = Regex.Replace(str, pattern, "", RegexOptions.IgnoreCase);
+            Console.WriteLine(str2);
+            Console.ReadKey();
+        }
+    }
+}
